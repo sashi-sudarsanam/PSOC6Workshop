f2ed741a7e05753f0552622c99b5390ad4d3f9a1
sashi-sudarsanam <119648641+sashi-sudarsanam@users.noreply.github.com>
2026-02-18 10:39:43 +0530

    Added breathing led project

---
+++ Labs/BreathingLED/main.c
@@ -0,0 +1,198 @@
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for Hello World Example using HAL APIs.
+*
+* Related Document: See README.md
+*
+*
+*******************************************************************************
+* Copyright 2022-2024, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cyhal.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+
+
+/*******************************************************************************
+* Macros
+*******************************************************************************/
+
+
+
+/*******************************************************************************
+* Global Variables
+*******************************************************************************/
+cy_stc_sysint_t PWM_IRQ_cfg=
+{
+/*.intrSrc =*/ PWM_IRQ,
+/*.intrPriority =*/ 2UL
+};
+
+
+
+/*******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+static void PWM_ISR()
+{
+    static bool state_off = 0;
+    static uint16 off_cnt = 0;
+    static uint32_t ramp_cnt = 0;	
+	
+	uint32_t interrupts = Cy_TCPWM_GetInterruptStatusMasked(PWM_HW, PWM_NUM);
+		/* Clear the interrupt */
+	Cy_TCPWM_ClearInterrupt(PWM_HW, PWM_NUM, interrupts);
+
+	if (0UL != (CY_TCPWM_INT_ON_TC & interrupts))
+	{
+        if (state_off == 0)
+        {
+			/* LED id ON */
+			ramp_cnt = ramp_cnt + 5;;
+			if (ramp_cnt >= Cy_TCPWM_PWM_GetPeriod0(PWM_HW, PWM_NUM))
+			{
+				ramp_cnt = 0; /* Reset ramp count */
+				off_cnt = Cy_TCPWM_PWM_GetCompare0Val(PWM_HW, PWM_NUM);
+				state_off = 1; /* Turn OFF led */
+			}
+			Cy_TCPWM_PWM_SetCompare0Val(PWM_HW, PWM_NUM, ramp_cnt);
+			
+		}
+		else 
+		{
+		    off_cnt--;
+		    if (off_cnt < 10)
+		    {
+				off_cnt = 0; /* Reset off count */
+				state_off = 0;
+			}
+			else 
+			{
+      			Cy_TCPWM_PWM_SetCompare0Val(PWM_HW, PWM_NUM, off_cnt);
+            }
+		}	
+
+	}
+}
+
+
+/*******************************************************************************
+* Function Name: main
+********************************************************************************
+* Summary:
+* This is the main function. It sets up a timer to trigger a periodic interrupt.
+* The main while loop checks for the status of a flag set by the interrupt and
+* toggles an LED at 1Hz to create an LED blinky. Will be achieving the 1Hz Blink
+* rate based on the The LED_BLINK_TIMER_CLOCK_HZ and LED_BLINK_TIMER_PERIOD
+* Macros,i.e. (LED_BLINK_TIMER_PERIOD + 1) / LED_BLINK_TIMER_CLOCK_HZ = X ,Here,
+* X denotes the desired blink rate. The while loop also checks whether the
+* 'Enter' key was pressed and stops/restarts LED blinking.
+*
+* Parameters:
+*  none
+*
+* Return:
+*  int
+*
+*******************************************************************************/
+int main(void)
+{
+    cy_rslt_t result;
+
+#if defined (CY_DEVICE_SECURE)
+    cyhal_wdt_t wdt_obj;
+
+    /* Clear watchdog timer so that it doesn't trigger a reset */
+    result = cyhal_wdt_init(&wdt_obj, cyhal_wdt_get_max_timeout_ms());
+    CY_ASSERT(CY_RSLT_SUCCESS == result);
+    cyhal_wdt_free(&wdt_obj);
+#endif /* #if defined (CY_DEVICE_SECURE) */
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init();
+
+    /* Board init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* Enable global interrupts */
+    __enable_irq();
+
+    /* Initialize retarget-io to use the debug UART port */
+    result = cy_retarget_io_init_fc(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX,
+            CYBSP_DEBUG_UART_CTS,CYBSP_DEBUG_UART_RTS,CY_RETARGET_IO_BAUDRATE);
+
+    /* retarget-io init failed. Stop program execution */
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
+    printf("\x1b[2J\x1b[;H");
+
+    printf("****************** "
+           "HAL: Hello World! Example "
+           "****************** \r\n\n");
+    if (CY_TCPWM_SUCCESS != Cy_TCPWM_PWM_Init(PWM_HW, PWM_NUM, &PWM_config))
+    {
+        /* Handle possible errors */
+    }
+    
+    /* set the interrupt line for PWM_HW */
+	if (CY_SYSINT_SUCCESS != Cy_SysInt_Init(&PWM_IRQ_cfg, &PWM_ISR))
+	{
+		return -1;
+	}
+	/* Enable system Interrupt */
+	NVIC_EnableIRQ((IRQn_Type) PWM_IRQ_cfg.intrSrc);
+    
+    /* Enable the initialized PWM */
+    Cy_TCPWM_PWM_Enable(PWM_HW, PWM_NUM);
+    /* Then start the PWM */
+    Cy_TCPWM_TriggerStart_Single(PWM_HW, PWM_NUM);
+
+    for (;;)
+    {
+
+    }
+}
+
+
+
+
+/* [] END OF FILE */
